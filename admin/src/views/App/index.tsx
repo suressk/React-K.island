085c48fc1a51b9f4be2976f49b7b51217a78bca6
Saul <saul.song@tuya.com>
2021-09-15 11:32:44 +0800

    init: initialize  project

---
+++ admin/src/views/App/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react'
+// import styles from './index.module.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
